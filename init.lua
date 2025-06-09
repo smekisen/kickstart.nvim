3c7982aa4b94315f19bdd8cdc34e2b1357cc8e0f
smekisen <zwilliamosz@gmail.com>
2025-06-09 15:10:48 +0200

    fuck blink

--- init.lua
+++ init.lua
@@ -708,6 +708,7 @@ require('lazy').setup({
   { -- Autocompletion
     'saghen/blink.cmp',
     event = 'VimEnter',
+    enabled = false,
     version = '1.*',
     dependencies = {
       -- Snippet Engine
@@ -780,7 +781,6 @@ require('lazy').setup({
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
       },
-
       sources = {
         default = { 'lsp', 'path', 'snippets', 'lazydev' },
         providers = {
--- lua/custom/plugins/coq_nvim.lua
+++ lua/custom/plugins/coq_nvim.lua
@@ -1,7 +1,7 @@
 return {
   'neovim/nvim-lspconfig', -- REQUIRED: for native Neovim LSP integration
   lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-  enabled = false,
+  -- enabled = false,
   dependencies = {
     -- main one
     { 'ms-jpq/coq_nvim', branch = 'coq' },
